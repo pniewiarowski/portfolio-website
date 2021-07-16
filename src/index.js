5ac611edbb82a3a3964f825d0aa8cf56cb51c3f6
CowMan <pawelniewiarowski2001@gmail.com>
2021-07-17 00:40:40 +0200

    Finish projects section

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const scrollerTitleManage = new ScrollerTitleManager([
   new ScrollerTitleManagerItem(0, 0.75, "paweł niewiarowski - portfolio"),
   new ScrollerTitleManagerItem(0.65, 1.65, "pn - projects"),
   new ScrollerTitleManagerItem(1.65, 2.65, "pn - skillset"),
-  new ScrollerTitleManagerItem(2.65, 3.25, "pn - about"),
-  new ScrollerTitleManagerItem(3.25, 5, "pn - contact")
+  new ScrollerTitleManagerItem(2.65, 3, "pn - about"),
+  new ScrollerTitleManagerItem(3.0, 5, "pn - contact")
 ]);
 
 hamburger.init();
