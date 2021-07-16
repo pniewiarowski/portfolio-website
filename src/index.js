ee32582477438092e8066d1f95169f73cf35c08f
CowMan <pawelniewiarowski2001@gmail.com>
2021-07-17 01:17:45 +0200

    Change sections height

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import "./index.scss";
 const hamburger = new Hamburger("[data-hamburger]", "[data-navbar]");
 const scrollerTitleManage = new ScrollerTitleManager([
   new ScrollerTitleManagerItem(0, 0.75, "paweł niewiarowski - portfolio"),
-  new ScrollerTitleManagerItem(0.65, 1.65, "pn - projects"),
-  new ScrollerTitleManagerItem(1.65, 2.65, "pn - skillset"),
-  new ScrollerTitleManagerItem(2.65, 3, "pn - about"),
-  new ScrollerTitleManagerItem(3.0, 5, "pn - contact")
+  new ScrollerTitleManagerItem(0.65, 1.25, "pn - projects"),
+  new ScrollerTitleManagerItem(1.25, 2.00, "pn - skillset"),
+  new ScrollerTitleManagerItem(2.00, 2.60, "pn - about"),
+  new ScrollerTitleManagerItem(2.60, 5.00, "pn - contact")
 ]);
 
 hamburger.init();
